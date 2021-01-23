6c411e04d641c87e766f81d6e8f44fdc4b00b0ae
Neel Smith <dnsmith.neel@gmail.com>
2021-01-23 15:53:43 -0500

    Rm erroneously added file

--- src/bugger.jl
+++
@@ -1,9 +0,0 @@
-using EditionBuilders
-using EzXML
-
-bldr = MidDiplomaticBuilder("Diplomatic edition", "dipl")
-doc = parsexml("<choice><sic>speling</sic><corr>spelling</corr></choice>")    
-n = root(doc)
-
-
-txt = wtoken(bldr, n)
